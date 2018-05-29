d85859f885def51dcc9c67d14acbb555e57aee45
Muhammad Salman <Muhammad Salman@DESKTOP-MP3B7LD>
2018-05-29 00:43:02 -0700

    Add project files.

---
+++ ColidColorlib/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ColidColorlib.MvcApplication" Language="C#" %>
---
+++ ColidColorlib/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ColidColorlib.Startup))]
+namespace ColidColorlib
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
