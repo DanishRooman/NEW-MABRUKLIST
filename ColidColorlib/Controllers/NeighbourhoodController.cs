6b1fac9e9cad0d3022d00630e1f73d84cae66170
Muhammad Salman <Muhammad Salman@DESKTOP-MP3B7LD>
2018-06-08 04:37:48 -0700

    Neighborhood and subtitle controllers added

---
+++ ColidColorlib/Controllers/NeighbourhoodController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ColidColorlib.Controllers
+{
+    public class NeighbourhoodController : Controller
+    {
+        [Authorize]
+        // GET: Neighbourhood
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
