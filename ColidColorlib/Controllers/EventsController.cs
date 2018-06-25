fcba58b5225e898530718b75462f4fc580038965
dell <dell@DESKTOP-J5P3N95>
2018-06-25 16:10:22 +0500

    Fixed users contact page

--- ColidColorlib/Controllers/EventsController.cs
+++ ColidColorlib/Controllers/EventsController.cs
@@ -5,6 +5,7 @@ using DataTransferObjects.Congregation;
 using DataTransferObjects.Event;
 using DataTransferObjects.Group;
 using DataTransferObjects.Neighbourhood;
+using DataTransferObjects.Users;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,9 +22,10 @@ namespace ColidColorlib.Controllers
         {
             return View();
         }
-        public ActionResult AddEvent() {
+        public ActionResult AddEvent()
+        {
             EventDTO evnt = new EventDTO();
-            using (MABRUKLISTEntities dbcontext=new MABRUKLISTEntities())
+            using (MABRUKLISTEntities dbcontext = new MABRUKLISTEntities())
             {
                 //Category
                 evnt.eventCategoryList = dbcontext.mblist_category.AsEnumerable().Select(x => new CategoryDto
@@ -32,27 +34,28 @@ namespace ColidColorlib.Controllers
                     Category = x.cat_name
                 }).ToList();
             };
-            return PartialView("_AddEvent",evnt) ;
+            return PartialView("_AddEvent", evnt);
         }
-        [HttpGet]
 
-       public ActionResult AddContact()
+        [HttpGet]
+        public ActionResult AddContact()
         {
-            
-            using (MABRUKLISTEntities dbcontaxt=new MABRUKLISTEntities()) {
+
+            using (MABRUKLISTEntities dbcontaxt = new MABRUKLISTEntities())
+            {
 
                 //Group
                 ViewBag.groups = dbcontaxt.mblist_group.AsEnumerable().Select(x => new SelectListItem
                 {
-                    Value=x.group_key.ToString(),
-                    Text=x.group_name.ToString()
+                    Value = x.group_key.ToString(),
+                    Text = x.group_name.ToString()
 
                 }).ToList();
                 //Congregation
                 ViewBag.congregations = dbcontaxt.mblist_congregation.AsEnumerable().Select(x => new SelectListItem
                 {
                     Value = x.congregation_key.ToString(),
-                    Text=x.congregation_name
+                    Text = x.congregation_name
                 }).ToList();
                 //Nieghbourhood
                 ViewBag.nieghbourhood = dbcontaxt.mblist_neighborhoods.AsEnumerable().Select(x => new SelectListItem
@@ -64,9 +67,35 @@ namespace ColidColorlib.Controllers
 
 
             };
-                return PartialView("_AddContact");
+            return PartialView("_AddContact");
         }
 
-      
+        public ActionResult UserDetails(int? group, int? Congregations, int? neighbourhood)
+        {
+            using (MABRUKLISTEntities dbcontext = new MABRUKLISTEntities())
+            {
+                var allusers = dbcontext.mblist_user_info.AsEnumerable();
+                if (group != null)
+                {
+                    allusers = allusers.Where(x => x.usr_group_key == group);
+                }
+                if (Congregations != null)
+                {
+                    allusers = allusers.Where(x => x.usr_congregation_key == Congregations);
+                }
+                if (neighbourhood != null)
+                {
+                    allusers = allusers.Where(x => x.usr_neighbourhood_key == neighbourhood);
+                }
+                var users = allusers.Select(x => new ContactUsersDto
+                {
+                    Users = x.usr_first_name + " " + x.usr_last_name,
+                    Action = "<input type='checkbox' value='" + x.AspNetUsers.Id + "'>"
+                }).ToList();
+                return Json(users, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+
     }
 }
--- ColidColorlib/Project Scripts/Event/Event.js
+++ ColidColorlib/Project Scripts/Event/Event.js
@@ -15,9 +15,47 @@
             }
         });
     };
+    var handleContactDatatable = function () {
+        var group = $("#ddlgroups").val();
+        group = group != undefined ? group : "";
+        var congregation = $("#ddlCongregations").val();
+        congregation = congregation != undefined ? congregation : "";
+        var neighbourhood = $("#ddlNieghbourhood").val();
+        neighbourhood = neighbourhood != undefined ? neighbourhood : "";
+        $.ajax({
+            url: "/Events/UserDetails",
+            data: { "group": group, "Congregations": congregation, "neighbourhood": neighbourhood },
+            type: "GET",
+            cache: false,
+            success: function (result) {
+                $('#tblContacts').DataTable({
+                    "destroy": true,
+                    "data": result,
+                    "columnDefs": [{
+                        orderable: false,
+                        targets: 1
+                    }],
+                    "columns": [
+                     {
+                         "data": "Users"
+                     },
+                     {
+                         "data": "Action"
+                     }
+                    ]
+                });
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                // $('#lblCommentsNotification').text("Error encountered while saving the comments.");
+            }
+        });
+    };
     return {
         initRenderContact: function () {
             handleRenderContact();
+        },
+        initContactDatatable: function () {
+            handleContactDatatable();
         },
     };
 }();
@@ -25,4 +63,6 @@
 
 $(function () {
     Event.initRenderContact();
+    Event.initContactDatatable();
 });
+
