f386d16cee873f7ee30fb95a769a21db50280eb4
dell <dell@DESKTOP-J5P3N95>
2018-06-26 11:56:55 +0500

    Event.js Fixed

--- ColidColorlib/Project Scripts/Event/Event.js
+++ ColidColorlib/Project Scripts/Event/Event.js
@@ -1,30 +1,68 @@
-﻿var Event=function(){
+﻿var Event = function () {
 
-    var handleDropdownList = function (e) {
-        debugger
-        //Ajax call
+    var handleRenderContact = function () {
         $.ajax({
             url: '/Events/AddContact',
             type: 'GET',
             dataType: 'HTML',
             data: {},
             success: function (result) {
-                $('.ddlList').html(result);
-                $('')
+                $('#step-2').empty();
+                $('#step-2').html(result);
             },
             error: function () {
                 console.log("Error");
             }
         });
     };
-    return {
-        initDropDownList: function (e) {
-        
-            handleDropdownList(e);
-        },
-
+    var handleContactDatatable = function () {
+        var group = $("#ddlgroups").val();
+        group = group != undefined ? group : "";
+        var congregation = $("#ddlCongregations").val();
+        congregation = congregation != undefined ? congregation : "";
+        var neighbourhood = $("#ddlNieghbourhood").val();
+        neighbourhood = neighbourhood != undefined ? neighbourhood : "";
+        $.ajax({
+            url: "/Events/UserDetails",
+            data: { "group": group, "Congregations": congregation, "neighbourhood": neighbourhood },
+            type: "GET",
+            cache: false,
+            success: function (result) {
+                $('#tblContacts').DataTable({
+                    "destroy": true,
+                    "data": result,
+                    "columnDefs": [{
+                        orderable: false,
+                        targets: 1
+                    }],
+                    "columns": [
+                     {
+                         "data": "Users"
+                     },
+                     {
+                         "data": "Action"
+                     }
+                    ]
+                });
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                // $('#lblCommentsNotification').text("Error encountered while saving the comments.");
+            }
+        });
     };
-
-
-
+    return {
+        initRenderContact: function () {
+            handleRenderContact();
+        },
+        initContactDatatable: function () {
+            handleContactDatatable();
+        },
+    };
 }();
+
+
+$(function () {
+    Event.initRenderContact();
+    Event.initContactDatatable();
+});
+
