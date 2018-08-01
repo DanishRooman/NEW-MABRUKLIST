1a12ae9eb0174ce2e3cf6c18ff3da778b5cd73a9
Muhammad Salman <Muhammad Salman@DESKTOP-MP3B7LD>
2018-08-01 08:50:01 -0700

    changes on previous button

--- ColidColorlib/Project Scripts/Event/Event.js
+++ ColidColorlib/Project Scripts/Event/Event.js
@@ -205,6 +205,13 @@
             });
         }
     };
+    var handlepreviousbutton = function () {
+        debugger
+        $("#previousstep").click();
+    };
+
+
+
 
     var handleCreateSubEvent = function (result) {
         if (result.key) {
@@ -572,6 +579,10 @@
         },
         initSubjectUpdated: function (data) {
             handleSubjectUpdated(data);
+        },
+        initpreviousbutton: function () {
+
+            handlepreviousbutton();
         },
     };
 }();
